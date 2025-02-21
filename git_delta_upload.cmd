88a7ba94e5161b5a2266592cc0d23d12b181393b
sql-analyst <s.ross_@hotmail.com>
2025-02-21 16:20:16 +1100

    Description of changes

--- git_delta_upload.cmd
+++
@@ -1,5 +0,0 @@
-cd /d C:\Users\sross\OneDrive\Documents\Consulting\Engineer_marketplace_flask
-git status
-git add .
-git commit -m "Description of changes"
-git push origin main
--- run_server.cmd
+++
@@ -1,4 +0,0 @@
-set PYTHON_PATH=C:\Users\sross\anaconda3\python.exe
-set SCRIPTS_PATH=C:\Users\sross\anaconda3\Scripts
-
-%PYTHON_PATH% app.py
